5dc137449b93c6ecd739b8c874025ef58ec56236
Vinicius Carvalho Guterres <vinicius.guterres@nordestealimentos.com.br>
2021-05-04 16:39:54 -0300

    A quit upgrade in the view map

--- function.js
+++ function.js
@@ -1,20 +1,23 @@
-const test = get();
+// the elements in the html that will recive latitude and logitude iss real time information
+const sectionLat = document.querySelector(".latitude span");
+const sectionLong = document.querySelector(".longitude span");
 
 // generated map from leaflet library
 const mymap = L.map('mapid', {
-    center: [test.latitude, test.longitude],
-    zoom: 5
+    center: [0, 0],
+    zoom: 3
 });
 
-var myIcon = L.icon({
+// setting the satellite icon that goes mark the iss position on the map
+var myIcon = L.icon({   
     iconUrl: 'iss.png',
-    iconSize: [50, 32],
+    iconSize: [100, 100],
     iconAnchor: [25, 16],
 });
 
+const marker = L.marker([0, 0], {icon: myIcon}).addTo(mymap);
 
-// const marker = L.marker([0, 0]).addTo(mymap);
-
+// setting the map tiles from Open Streetmap
 const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
@@ -24,25 +27,22 @@ const tiles = L.tileLayer(tileUrl, {attribution} );
 tiles.addTo(mymap);
 
 
+// getting the coordinates from ISS api and update it every 1 second
 setInterval(
     async function get() {
-
-        const sectionLat = document.querySelector(".latitude span");
-        const sectionLong = document.querySelector(".longitude span");
 
         const requestUrl = "https://api.wheretheiss.at/v1/satellites/25544";
 
         const response = await fetch(requestUrl);
 
-        const data = await response.json();
+        const data = await response.json(); 
         
         const {latitude, longitude} = data;
 
-        // passing the coordenates of my station to my map make a maker on it
-        // marker.setLatLng([latitude, longitude]);
-        L.marker([latitude, longitude]).addTo(mymap)
-        mymap.center = [latitude, longitude];
+        mymap.setView([latitude, longitude]);
 
+        // passing the coordenates of my station to my map make a maker on it
+        marker.setLatLng([latitude, longitude]);       
 
         sectionLat.textContent = latitude;
 
